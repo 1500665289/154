18acfc2da04a1c4f9ccdbcc79e7e613e03ca1b26
1500665289 <152504086+1500665289@users.noreply.github.com>
2025-11-23 12:03:16 +0800

    1.1

---
+++ Scripts/XXZS_QH.lua
@@ -0,0 +1,67 @@
+﻿local tbTable = GameMain:GetMod("XXZS_QH");
+--attrStrList 依次对应为以下项目
+--[[
+		None,
+		AttackPower,
+		LingRecover,
+		MaxLing,
+		FlySpeed,
+		RotSpeed,
+		KnockBackAddition,
+		KnockBackResistance,
+		Scale,
+		TailLenght,
+		AttackRate,
+		_FightFabaoEnd,
+		MoveSpeed,
+		_FlyFabaoEnd
+--]]
+
+function tbTable:QH(attrCS,attrMin,attrMax,attrAddv,attrAddp)
+
+	--*具体名称，请参考游戏内的称呼进行修改
+	local attrStrList = {
+	'None',
+	'法宝威力',
+	'回灵速度',
+	'最大灵气',
+	'飞行速度',
+	'回旋速度',
+	'击退强度',
+	'击退抵抗',
+	'大小',
+	'拖尾长度',
+	'攻击间隔',
+	'_FightFabaoEnd',
+	'移动速度',
+	'_FlyFabaoEnd'};
+	local myNpc = world:GetSelectThing();
+	local fabao = myNpc:GetFirstAtkFabao();
+	if fabao ~=nil then
+		local attr = fabao.Fabao:GetProperty(attrCS);
+		local attrCSStr = tostring(attrCS);
+		local index = tonumber(string.match(attrCSStr,"%d+"));
+		local attrStr = (attrStrList[index+1]);
+		
+		if attr <=attrMax and attr >=attrMin then
+			local value = attr;
+			if attrAddv ~=nil and attrAddv > 0 then
+				value = attr + attrAddv;
+			else
+				value = attr + attr*attrAddp;
+			end
+			fabao.Fabao:SetProperty(attrCS,value);
+			world:ShowMsgBox('法宝的 [color=#FF0000]'..attrStr..'[/color] 强化完成\n'..'强化前'..math.ceil(attr)..'\n'..'强化后'..math.ceil(value));
+		else
+			world:ShowMsgBox('法宝的'..attrStr..'属性异常！\n限制为 最高'..tostring(attrMax)..' 最低'..tostring(attrMin));
+		end
+	else
+		world:ShowMsgBox('NPC没有装备法宝！')
+	end
+end
+
+--[[
+local attrCS = CS.XiaWorld.Fight.g_emFaBaoP.AttackPower;
+GameMain:GetMod("XXZS_QH"):QH(attrCS,1,800,0,0.1);
+GameMain:GetMod("XXZS_QH"):QH(attrCS,1,800,100,0);
+-]]
---
+++ Scripts/XiaoQi.lua
@@ -0,0 +1,72 @@
+﻿local XiaoQi = GameMain:GetMod("XiaoQi");
+
+
+function XiaoQi:OnAfterLoad()
+	local file = "Settings/Ui/Help/Help.xml"
+	local ModPaths = XiaoQi:GetPaths("Settings/Ui/Help/Help.xml");
+	for k,v in pairs(ModPaths) do
+		local PathData = v;
+		local text = CS.GFileUtil.ReadTXT(PathData, true);
+		if text.Length == 0 then
+			break;
+		end
+		local xmlSerializer = CS.System.Xml.Serialization.XmlSerializer(typeof(CS.XiaWorld.UIHelpDefs));
+		local uihelpDefs = xmlSerializer:Deserialize(CS.System.IO.StringReader(text));
+		XiaoQi:LoadHelp(uihelpDefs)
+	end
+end
+
+function XiaoQi:LoadHelp(defs)
+	xlua.private_accessible(CS.XiaWorld.UILogicMgr);
+	for k,v in pairs(defs.Datas) do
+		local uihelpDef = v;
+		if CS.XiaWorld.UILogicMgr.Instance.mapHelpData:ContainsKey(v.Name) then
+			CS.XiaWorld.UILogicMgr.Instance.mapHelpData:Remove(v.Name);
+		end
+		CS.XiaWorld.UILogicMgr.Instance.mapHelpData:Add(v.Name,v);
+	end
+end
+
+function XiaoQi:GetPaths(filepath)
+	local list = {}
+	local Mods = CS.ModsMgr.Instance.Mods
+	for k,v in pairs(Mods) do
+		if v.IsActive then
+			local path2 = v.Path .. "/" .. filepath;
+			if CS.System.IO.File.Exists(path2) then
+				table.insert(list,path2)
+			end
+		end
+	end
+	return list;
+end
+
+function XiaoQi:OnEnter()
+	CS.Wnd_Help.Instance:Show();
+	CS.Wnd_Help.Instance:Hide();
+	CS.Wnd_Help.Instance.contentPane.m_Text.m_title.onClickLink:Add(XiaoQi.onClickLink); 
+end
+
+function XiaoQi.onClickLink(Context)
+	local link = Context.data;
+	if string.find(link,"http://") or string.find(link,"https://") then
+		CS.UnityEngine.Application.OpenURL(Context.data)
+	end
+	if string.find(link,"tencent://") and CS.System.Diagnostics.Process.GetProcessesByName("QQ").Length then
+		CS.UnityEngine.Application.OpenURL(Context.data)
+	end
+end
+
+function XiaoQi:ShowHelp(Context)
+	CS.Wnd_Help.Instance:Show();
+	local list = CS.Wnd_Help.Instance.contentPane.m_menu;
+	for i=0, list.numChildren -1 do
+		local item = list:GetChildAt(i);
+		if item.m_n19.data == "XiaoQi_Main" then
+			item.m_n19.onClick:Call();
+		end
+	end
+	local helpDef = UILogicMgr:GetHelpDef("XiaoQi_1");
+	CS.Wnd_Help.Instance.contentPane.m_Text.text = helpDef.Text;
+	CS.Wnd_Help.Instance.contentPane.m_Text.scrollPane:ScrollTop();
+end
