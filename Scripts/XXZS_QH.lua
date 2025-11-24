82485247e29448ba823f30ecd1a25353738798a2
1500665289 <152504086+1500665289@users.noreply.github.com>
2025-11-24 15:35:02 +0800

    1

--- Scripts/XXZS_QH.lua
+++ Scripts/XXZS_QH.lua
@@ -17,51 +17,91 @@
 		_FlyFabaoEnd
 --]]
 
-function tbTable:QH(attrCS,attrMin,attrMax,attrAddv,attrAddp)
-
-	--*具体名称，请参考游戏内的称呼进行修改
-	local attrStrList = {
-	'None',
-	'法宝威力',
-	'回灵速度',
-	'最大灵气',
-	'飞行速度',
-	'回旋速度',
-	'击退强度',
-	'击退抵抗',
-	'大小',
-	'拖尾长度',
-	'攻击间隔',
-	'_FightFabaoEnd',
-	'移动速度',
-	'_FlyFabaoEnd'};
-	local myNpc = world:GetSelectThing();
-	local fabao = myNpc:GetFirstAtkFabao();
-	if fabao ~=nil then
-		local attr = fabao.Fabao:GetProperty(attrCS);
-		local attrCSStr = tostring(attrCS);
-		local index = tonumber(string.match(attrCSStr,"%d+"));
-		local attrStr = (attrStrList[index+1]);
-		
-		if attr <=attrMax and attr >=attrMin then
-			local value = attr;
-			if attrAddv ~=nil and attrAddv > 0 then
-				value = attr + attrAddv;
-			else
-				value = attr + attr*attrAddp;
-			end
-			fabao.Fabao:SetProperty(attrCS,value);
-			world:ShowMsgBox('法宝的 [color=#FF0000]'..attrStr..'[/color] 强化完成\n'..'强化前'..math.ceil(attr)..'\n'..'强化后'..math.ceil(value));
-		else
-			world:ShowMsgBox('法宝的'..attrStr..'属性异常！\n限制为 最高'..tostring(attrMax)..' 最低'..tostring(attrMin));
-		end
-	else
-		world:ShowMsgBox('NPC没有装备法宝！')
-	end
+function tbTable:QH(attrCS, attrMin, attrMax, attrAddv, attrAddp)
+    --*具体名称，请参考游戏内的称呼进行修改
+    local attrStrList = {
+        'None',
+        '法宝威力',
+        '回灵速度',
+        '最大灵气',
+        '飞行速度',
+        '回旋速度',
+        '击退强度',
+        '击退抵抗',
+        '大小',
+        '拖尾长度',
+        '攻击间隔',
+        '_FightFabaoEnd',
+        '移动速度',
+        '_FlyFabaoEnd'
+    };
+    
+    -- 增加空值检查
+    if attrCS == nil or attrMin == nil or attrMax == nil then
+        world:ShowMsgBox('强化参数错误！')
+        return
+    end
+    
+    local myNpc = world:GetSelectThing();
+    if myNpc == nil then
+        world:ShowMsgBox('没有选中NPC！')
+        return
+    end
+    
+    local fabao = myNpc:GetFirstAtkFabao();
+    if fabao == nil or fabao.Fabao == nil then
+        world:ShowMsgBox('NPC没有装备法宝！')
+        return
+    end
+    
+    local attr = fabao.Fabao:GetProperty(attrCS);
+    local attrCSStr = tostring(attrCS);
+    local index = tonumber(string.match(attrCSStr, "%d+"));
+    
+    -- 增加索引范围检查
+    if index == nil or index < 0 or index >= #attrStrList then
+        world:ShowMsgBox('属性类型错误！')
+        return
+    end
+    
+    local attrStr = attrStrList[index + 1] or '未知属性';
+    
+    if attr <= attrMax and attr >= attrMin then
+        local value = attr;
+        
+        -- 改进数值计算逻辑
+        if attrAddv ~= nil and attrAddv > 0 then
+            value = attr + attrAddv;
+        elseif attrAddp ~= nil and attrAddp > 0 then
+            value = attr + attr * attrAddp;
+        else
+            world:ShowMsgBox('强化参数错误！请提供有效的强化值或百分比。')
+            return
+        end
+        
+        -- 限制最大值，防止溢出
+        if value > attrMax then
+            value = attrMax
+        end
+        
+        fabao.Fabao:SetProperty(attrCS, value);
+        
+        -- 格式化显示数值
+        local beforeValue = math.ceil(attr * 100) / 100  -- 保留2位小数
+        local afterValue = math.ceil(value * 100) / 100
+        
+        world:ShowMsgBox('法宝的 [color=#FF0000]'..attrStr..'[/color] 强化完成\n'..
+                        '强化前: '..tostring(beforeValue)..'\n'..
+                        '强化后: '..tostring(afterValue));
+    else
+        world:ShowMsgBox('法宝的'..attrStr..'属性异常！\n'..
+                        '当前值: '..tostring(math.ceil(attr * 100) / 100)..'\n'..
+                        '限制范围: '..tostring(attrMin)..' - '..tostring(attrMax));
+    end
 end
 
 --[[
 local attrCS = CS.XiaWorld.Fight.g_emFaBaoP.AttackPower;
 GameMain:GetMod("XXZS_QH"):QH(attrCS,1,800,0,0.1);
 GameMain:GetMod("XXZS_QH"):QH(attrCS,1,800,100,0);
--]]
+--]]
--- Scripts/XiaoQi.lua
+++ Scripts/XiaoQi.lua
@@ -1,72 +1,114 @@
 ﻿local XiaoQi = GameMain:GetMod("XiaoQi");
 
-
 function XiaoQi:OnAfterLoad()
-	local file = "Settings/Ui/Help/Help.xml"
-	local ModPaths = XiaoQi:GetPaths("Settings/Ui/Help/Help.xml");
-	for k,v in pairs(ModPaths) do
-		local PathData = v;
-		local text = CS.GFileUtil.ReadTXT(PathData, true);
-		if text.Length == 0 then
-			break;
-		end
-		local xmlSerializer = CS.System.Xml.Serialization.XmlSerializer(typeof(CS.XiaWorld.UIHelpDefs));
-		local uihelpDefs = xmlSerializer:Deserialize(CS.System.IO.StringReader(text));
-		XiaoQi:LoadHelp(uihelpDefs)
-	end
+    -- 移除未使用的file变量
+    local ModPaths = XiaoQi:GetPaths("Settings/Ui/Help/Help.xml");
+    for k, v in pairs(ModPaths) do
+        local PathData = v;
+        local text = CS.GFileUtil.ReadTXT(PathData, true);
+        if text == nil or text.Length == 0 then
+            break;
+        end
+        local xmlSerializer = CS.System.Xml.Serialization.XmlSerializer(typeof(CS.XiaWorld.UIHelpDefs));
+        local uihelpDefs = xmlSerializer:Deserialize(CS.System.IO.StringReader(text));
+        if uihelpDefs ~= nil then
+            XiaoQi:LoadHelp(uihelpDefs)
+        end
+    end
 end
 
 function XiaoQi:LoadHelp(defs)
-	xlua.private_accessible(CS.XiaWorld.UILogicMgr);
-	for k,v in pairs(defs.Datas) do
-		local uihelpDef = v;
-		if CS.XiaWorld.UILogicMgr.Instance.mapHelpData:ContainsKey(v.Name) then
-			CS.XiaWorld.UILogicMgr.Instance.mapHelpData:Remove(v.Name);
-		end
-		CS.XiaWorld.UILogicMgr.Instance.mapHelpData:Add(v.Name,v);
-	end
+    if defs == nil or defs.Datas == nil then
+        return
+    end
+    
+    xlua.private_accessible(CS.XiaWorld.UILogicMgr);
+    local uiLogicMgr = CS.XiaWorld.UILogicMgr.Instance
+    if uiLogicMgr == nil or uiLogicMgr.mapHelpData == nil then
+        return
+    end
+    
+    -- 使用ipairs确保顺序遍历，避免非连续索引问题
+    for i = 0, defs.Datas.Count - 1 do
+        local uihelpDef = defs.Datas[i]
+        if uihelpDef ~= nil and uihelpDef.Name ~= nil then
+            if uiLogicMgr.mapHelpData:ContainsKey(uihelpDef.Name) then
+                uiLogicMgr.mapHelpData:Remove(uihelpDef.Name);
+            end
+            uiLogicMgr.mapHelpData:Add(uihelpDef.Name, uihelpDef);
+        end
+    end
 end
 
 function XiaoQi:GetPaths(filepath)
-	local list = {}
-	local Mods = CS.ModsMgr.Instance.Mods
-	for k,v in pairs(Mods) do
-		if v.IsActive then
-			local path2 = v.Path .. "/" .. filepath;
-			if CS.System.IO.File.Exists(path2) then
-				table.insert(list,path2)
-			end
-		end
-	end
-	return list;
+    local list = {}
+    local Mods = CS.ModsMgr.Instance.Mods
+    if Mods == nil then
+        return list
+    end
+    
+    for k, v in pairs(Mods) do
+        if v.IsActive then
+            local path2 = v.Path .. "/" .. filepath;
+            if CS.System.IO.File.Exists(path2) then
+                table.insert(list, path2)
+            end
+        end
+    end
+    return list;
 end
 
 function XiaoQi:OnEnter()
-	CS.Wnd_Help.Instance:Show();
-	CS.Wnd_Help.Instance:Hide();
-	CS.Wnd_Help.Instance.contentPane.m_Text.m_title.onClickLink:Add(XiaoQi.onClickLink); 
+    if CS.Wnd_Help.Instance == nil then
+        return
+    end
+    
+    CS.Wnd_Help.Instance:Show();
+    CS.Wnd_Help.Instance:Hide();
+    
+    if CS.Wnd_Help.Instance.contentPane ~= nil and 
+       CS.Wnd_Help.Instance.contentPane.m_Text ~= nil and 
+       CS.Wnd_Help.Instance.contentPane.m_Text.m_title ~= nil then
+        CS.Wnd_Help.Instance.contentPane.m_Text.m_title.onClickLink:Add(XiaoQi.onClickLink); 
+    end
 end
 
 function XiaoQi.onClickLink(Context)
-	local link = Context.data;
-	if string.find(link,"http://") or string.find(link,"https://") then
-		CS.UnityEngine.Application.OpenURL(Context.data)
-	end
-	if string.find(link,"tencent://") and CS.System.Diagnostics.Process.GetProcessesByName("QQ").Length then
-		CS.UnityEngine.Application.OpenURL(Context.data)
-	end
+    if Context == nil or Context.data == nil then
+        return
+    end
+    
+    local link = Context.data;
+    if string.find(link, "http://") or string.find(link, "https://") then
+        CS.UnityEngine.Application.OpenURL(Context.data)
+    end
+    if string.find(link, "tencent://") and CS.System.Diagnostics.Process.GetProcessesByName("QQ").Length > 0 then
+        CS.UnityEngine.Application.OpenURL(Context.data)
+    end
 end
 
 function XiaoQi:ShowHelp(Context)
-	CS.Wnd_Help.Instance:Show();
-	local list = CS.Wnd_Help.Instance.contentPane.m_menu;
-	for i=0, list.numChildren -1 do
-		local item = list:GetChildAt(i);
-		if item.m_n19.data == "XiaoQi_Main" then
-			item.m_n19.onClick:Call();
-		end
-	end
-	local helpDef = UILogicMgr:GetHelpDef("XiaoQi_1");
-	CS.Wnd_Help.Instance.contentPane.m_Text.text = helpDef.Text;
-	CS.Wnd_Help.Instance.contentPane.m_Text.scrollPane:ScrollTop();
+    if CS.Wnd_Help.Instance == nil then
+        return
+    end
+    
+    CS.Wnd_Help.Instance:Show();
+    local list = CS.Wnd_Help.Instance.contentPane.m_menu;
+    if list ~= nil then
+        for i = 0, list.numChildren - 1 do
+            local item = list:GetChildAt(i);
+            if item ~= nil and item.m_n19 ~= nil and item.m_n19.data == "XiaoQi_Main" then
+                item.m_n19.onClick:Call();
+            end
+        end
+    end
+    
+    -- 修正UILogicMgr引用
+    local helpDef = CS.XiaWorld.UILogicMgr.Instance:GetHelpDef("XiaoQi_1");
+    if helpDef ~= nil and CS.Wnd_Help.Instance.contentPane.m_Text ~= nil then
+        CS.Wnd_Help.Instance.contentPane.m_Text.text = helpDef.Text;
+        if CS.Wnd_Help.Instance.contentPane.m_Text.scrollPane ~= nil then
+            CS.Wnd_Help.Instance.contentPane.m_Text.scrollPane:ScrollTop();
+        end
+    end
 end
